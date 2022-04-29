563550384c47441dd5c403a8dda62199376712e4
mariajoana <gokalp.maryam@gmail.com>
2022-04-29 11:32:05 +0100

    day1 Basic navigations

--- src/test/java/com/cydeo/tests/day1_selenium_intro/BasicNavigations.java
+++ src/test/java/com/cydeo/tests/day1_selenium_intro/BasicNavigations.java
@@ -12,8 +12,11 @@ public class BasicNavigations {
         WebDriverManager.chromedriver().setup();
 
         //2-Create instance of the "Selenium WebDriver" Implementation will appear on the below note
-        //The line below it will open an empty browser
+        //The line below it will open my browser
         WebDriver driver = new ChromeDriver();
+
+        // place driver line after my ChromeDriver & it will maximize the browser size
+        driver.manage().window().maximize();
 
         //3- Go to "https://www.tesla.com"
         driver.get("https://www.tesla.com");
@@ -22,7 +25,6 @@ public class BasicNavigations {
         // by using Thread.sleep so when is get compiler error on sleep u need to handle the exception by click on add exception to the method signature
         // Java run the code top 2 bottom & left 2 right so when it comes to ChromeDrive line it will open an empty browser
         // so every time when u run specific code it generate specific id for that specific run time, it opened a new browser.
-
         // get the title of the page & print URL after the titled
         String currentTitle = driver.getTitle();
         System.out.println("currentTitle = " + currentTitle);
@@ -42,7 +44,7 @@ public class BasicNavigations {
         //Stop code execution for 3 seconds
         Thread.sleep(3000);
 
-       // use selenium to navigate forward
+        // use selenium to navigate forward
         //selenium session is created everytime when u run a selenium code
         driver.navigate().forward();
 
@@ -66,14 +68,11 @@ public class BasicNavigations {
         System.out.println("currentTitle = " + currentTitle);
 
         // Get the current URL using Selenium 4 me 2 get 2 different URL Tesla & Google URL
+        //Summary try to get title & URL from 2 different pages  & You can inspect the web page to see the changes
+        // create one string call one method 2 print one value
         currentURL = driver.getCurrentUrl();
 
         System.out.println("currentURL = " + currentUrl);
-
-        //Summary try to get title & URL from 2 different pages  & You can inspect the web page to see the changes
-        // create one string call one method 2 print one value
-
-
 
 
 
