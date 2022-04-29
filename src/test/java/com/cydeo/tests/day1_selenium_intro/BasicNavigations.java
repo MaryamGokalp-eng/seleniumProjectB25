50badaf23265363d60828e03a9a1f1018826d268
mariajoana <gokalp.maryam@gmail.com>
2022-04-29 10:44:23 +0100

    day1 Basic navigations

--- src/test/java/com/cydeo/tests/day1_selenium_intro/BasicNavigations.java
+++ src/test/java/com/cydeo/tests/day1_selenium_intro/BasicNavigations.java
@@ -19,10 +19,21 @@ public class BasicNavigations {
         driver.get("https://www.tesla.com");
 
         //Stop code execution for 3 seconds that's how we are going to be able to slow the selenium down
-        // by using Thread.sleep so when is get compiler error on sleep u need to handle on the lazy way for no
-        // To handle exception click on add exception to the method signature
+        // by using Thread.sleep so when is get compiler error on sleep u need to handle the exception by click on add exception to the method signature
         // Java run the code top 2 bottom & left 2 right so when it comes to ChromeDrive line it will open an empty browser
-        // so every time when u run specific code it generate specific id for that specific run time, it open a new browser.
+        // so every time when u run specific code it generate specific id for that specific run time, it opened a new browser.
+
+        // get the title of the page
+        String currentTitle = driver.getTitle();
+        System.out.println("currentTitle = " + currentTitle);
+
+        String currentUrl = driver.getCurrentUrl();
+        System.out.println("currentURL = " + currentUrl);
+
+
+        String currentURL = driver.getCurrentUrl();
+        System.out.println("currentURL = " + currentURL);
+
 
         //Stop code for 3 seconds
         Thread.sleep(3000);
@@ -34,8 +45,36 @@ public class BasicNavigations {
         Thread.sleep(3000);
 
        // use selenium to navigate forward
-        //selenium session is create everytime when u run a selenium code
+        //selenium session is created everytime when u run a selenium code
         driver.navigate().forward();
+
+        // stop code execution for 3seconds in each step
+        Thread.sleep(3000);
+
+        // use selenium to navigate refresh
+        driver.navigate().refresh();
+
+        // stop code execution for 3seconds in each step
+        Thread.sleep(3000);
+
+        // use navigate().to(); I need to call my title again by rea-signed currentTitle equal diver.getTitle
+        //the Selenium check the title & return the value, if I am in GooglePage GetGoogleTitle, return & print
+        driver.navigate().to("https://www.google.com");
+
+        // To get the two title I should print 2x the currentTitle after getting the Google page
+        currentTitle = driver.getTitle();
+
+        //System.out.println("driver.getTitle() = " + driver.getTitle());
+        System.out.println("currentTitle = " + currentTitle);
+
+        // Get the current URL using Selenium & print ou to the beauty
+
+
+        System.out.println("currentURL = " + currentUrl);
+
+
+
+
 
 
     }
