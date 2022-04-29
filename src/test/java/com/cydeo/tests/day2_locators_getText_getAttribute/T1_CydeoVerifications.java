108daa0787d62d9aa4e55239d6457afa98e94a71
mariajoana <gokalp.maryam@gmail.com>
2022-04-29 23:23:04 +0100

    day2 Task1_CydeoVerifications

--- src/test/java/com/cydeo/tests/day2_locators_getText_getAttribute/T1_CydeoVerifications.java
+++ src/test/java/com/cydeo/tests/day2_locators_getText_getAttribute/T1_CydeoVerifications.java
@@ -3,13 +3,12 @@ package com.cydeo.tests.day2_locators_getText_getAttribute;
 public class T1_CydeoVerifications {
     public static void main(String[] args) {
 
-// TC #1: Cydeo practice tool verifications
-//1. Open Chrome browser
-//2. Go to https://practice.cydeo.com
-//3. Verify URL contains
-//Expected: cydeo
+// TC #1: Cydeo practice tool verifications.
+//2. Go to https://practice.cydeo.com.
+//3. Verify URL contains.
+//Expected: cydeo.
 //4. Verify title:
-//Expected: Practice
+//Expected: Practice.
 
 
 
