e8407a34fdb944c54b7777424fda8c80d3b91f40
mariajoana <gokalp.maryam@gmail.com>
2022-05-01 17:50:18 +0100

    Day3 T2_getText_getAttribute

---
+++ src/test/java/com/cydeo/tests/day3_CssSellector_xpath/T2_getText_getAttribute.java
@@ -0,0 +1,65 @@
+package com.cydeo.tests.day3_CssSellector_xpath;
+
+import com.cydeo.tests.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class T2_getText_getAttribute {
+
+    public static void main(String[] args) {
+        //TC #2: NextBaseCRM, locators, getText(), getAttribute() practice
+        //1- Open a Chrome browser
+        //U must do manual testing 1st 2 make sure everything is fine 4 U to be able to automate ur Test On the WebPage
+        WebDriver driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+
+        //2- Go to: https://login1.nextbasecrm.com/
+        driver.get("https://login1.nextbasecrm.com/");
+
+        //3- Verify “remember me” label text is as expected:
+        //Expected: Remember me on this computer
+        WebElement rememberMeLabel = driver.findElement(By.className("login-item-checkbox-label"));
+
+        String expectedRememberMeLabel= "Remember me on this computer";//To VerifyApply regular label verification
+        String actualRememberMeLabel = rememberMeLabel.getText();//2rd Verification
+
+        if (actualRememberMeLabel.equals(expectedRememberMeLabel)){
+            System.out.println("Label verification PASSED!");
+        }else{
+            System.out.println("Label verification FAILED!!!");
+        }
+
+        //4- Verify “forgot password” link text is as expected:
+        //Expected: Forgot your password?
+        WebElement forgotPasswordLink = driver.findElement(By.className("login-link-forgot-pass"));
+       String expectedForgotPasswordLinkText = "FORGOT YOUR PASSWORD?";//Remember ifFail COZ WeJust compareValue 2gether
+       String actualForgotPasswordLinkText = forgotPasswordLink.getText();//If Fail COZ WeJust compareValue together
+
+        if (actualForgotPasswordLinkText.equals(expectedForgotPasswordLinkText)){
+            System.out.println("Forgot password link verification PASSED!");
+        }else {
+            System.out.println("actualForgotPasswordLinkText = " + actualForgotPasswordLinkText);
+            System.out.println("expectedForgotPasswordLinkText = " + expectedForgotPasswordLinkText);
+
+            System.out.println("Forgot password link verification FAILED!!!");
+        }
+
+        //5- Verify “forgot password” href attribute’s value contains expected:
+        //Expected: forgot_password=yes
+        String expectedInHref = "forgot_password=yes";
+        String actualHrefAttributeValue = forgotPasswordLink.getAttribute("href");
+
+        System.out.println("actualHrefAttributeValue = " + actualHrefAttributeValue);
+
+        if (actualHrefAttributeValue.contains(expectedInHref)){
+            System.out.println("HREF attribute value verification PASSED!");
+        }else{
+            System.out.println("HREF attribute value verification FAILED!!!");
+        }
+
+
+
+    }
+
+}
