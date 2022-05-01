b1fa1a16fa7aef061fbfe7db3175d9f661730bf0
mariajoana <gokalp.maryam@gmail.com>
2022-05-01 10:19:53 +0100

    day2 T4_LibraryLoginPage

--- src/test/java/com/cydeo/tests/day2_locators_getText_getAttribute/T4_LibraryLoginPage.java
+++ src/test/java/com/cydeo/tests/day2_locators_getText_getAttribute/T4_LibraryLoginPage.java
@@ -18,10 +18,16 @@ public class T4_LibraryLoginPage {
         driver.get("https://library2.cybertekschool.com/login.html");
 
         //3. Enter username: “incorrect@email.com”
+        // The webElement on your inspect from the web that accept input from the user are? INPUT TAG
+        //From the WebElement inspect page that accept value from the user both email & password
+        //They both have type,ID & class attribute & to log in on libraryWebpage, using "form-control"input 4 the email
+        //It's find & return 2 me the 1st matching one result,I need to add (By.className plus ("form-control"));
+        // to locate the username. Next type usernameInput.sendKeys("incorrect@email.com");
         WebElement usernameInput = driver.findElement(By.className("form-control"));
         usernameInput.sendKeys("incorrect@email.com");
 
         //4. Enter password: “incorrect password”
+        //Use "inputPassword" for the password
         WebElement passwordInput = driver.findElement(By.id("inputPassword"));
         passwordInput.sendKeys("incorrect password");
 
