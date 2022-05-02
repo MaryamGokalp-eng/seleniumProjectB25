b8de39c89fd01bdf9e36a33e6a1da25924ba6c62
mariajoana <gokalp.maryam@gmail.com>
2022-05-02 23:18:51 +0100

    Day2_Homework: TC2_ZeroBankHeaderVerification

---
+++ src/test/java/com/cydeo/reviewWithOscar/HomeWorkTask/TC2_ZeroBankHeaderVerification.java
@@ -0,0 +1,40 @@
+package com.cydeo.reviewWithOscar.HomeWorkTask;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TC2_ZeroBankHeaderVerification {
+    public static void main(String[] args) {
+
+        //TC #2: Zero Bank header verification
+        //1. Open Chrome browser
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+        //2. Go to http://zero.webappsecurity.com/login.html
+        driver.get("http://zero.webappsecurity.com/login.html");
+
+        //3. Verify header text
+        //Expected: “Log in to ZeroBank”
+        String expectedTitle = "Log in to ZeroBank";
+        String actualTitle = driver.getTitle();
+
+        if (actualTitle.equals(expectedTitle)){
+            System.out.println("PASS");
+        }else{
+            System.out.println("FAILED");
+        }
+
+    }
+}
+
+/*
+
+        //TC #2: Zero Bank header verification
+        //1. Open Chrome browser
+        //2. Go to http://zero.webappsecurity.com/login.html
+        //3. Verify header text
+        //Expected: “Log in to ZeroBank”
+ */
