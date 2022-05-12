18971c2792b0e900bb94adffd3e4360699d70e54
mariajoana <gokalp.maryam@gmail.com>
2022-05-12 11:35:29 +0100

    day5_T1_StaleElementRefEx

--- src/test/java/com/cydeo/tests/day5_testing_Intro_dropdowns/T1_StaleElementRefEx.java
+++ src/test/java/com/cydeo/tests/day5_testing_Intro_dropdowns/T1_StaleElementRefEx.java
@@ -1,35 +1,59 @@
 package com.cydeo.tests.day5_testing_Intro_dropdowns;
 
+import com.cydeo.reviewWithOscar.week2.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.StaleElementReferenceException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.concurrent.TimeUnit;
+
 public class T1_StaleElementRefEx {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
-        //XPATH PRACTICES
-        //DO NOT USE ANY DEVELOPER TOOLS TO GET ANY LOCATORS.
         //TC #1: StaleElementReferenceException handling
         //1. Open Chrome browser
-
+        WebDriver driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
         //2. Go to https://practice.cydeo.com/add_remove_elements/
+        driver.get("https://practice.cydeo.com/add_remove_elements/");
+
 
 
         //3. Click to “Add Element” button
+        //button[@onclick='addElement()']
+        //GoalIsToLocateTheWebElementClickOnAddElement+Inspect LocateTheWebElementThatReturnsMe
+        WebElement addElementBtn = driver.findElement(By.xpath("//button[.='Add Element']"));
 
+        Thread.sleep(2000);
+        addElementBtn.click();
 
         //4. Verify “Delete” button is displayed after clicking.
+        WebElement deleteButton = driver.findElement(By.xpath("//button[@class='added-manually']"));
 
+        System.out.println("deleteButton.isDisplayed() = " + deleteButton.isDisplayed());
 
         //5. Click to “Delete” button.
-
+        deleteButton.click();
 
         //6. Verify “Delete” button is NOT displayed after clicking.
 
+        try {
 
-        //USE XPATH LOCATOR FOR ALL WEBELEMENT LOCATORS
+            System.out.println("deleteButton.isDisplayed() = " + deleteButton.isDisplayed());
 
+        }catch (StaleElementReferenceException e){
 
+            System.out.println("-->StaleElementReferenceException exception is thrown");
+            System.out.println("-->This means the web element is completely deleted from the page");
+            System.out.println("deleteButton.isDisplayed() = false");
 
+        }
 
+        driver.close();
     }
 }
 
