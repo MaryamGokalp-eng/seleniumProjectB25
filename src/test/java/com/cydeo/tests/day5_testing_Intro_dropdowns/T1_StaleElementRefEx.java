c12bf88cdffe78b156fc4e921980ce1954c846e1
mariajoana <gokalp.maryam@gmail.com>
2022-05-12 12:46:23 +0100

    day5_T1_StaleElementRefEx

--- src/test/java/com/cydeo/tests/day5_testing_Intro_dropdowns/T1_StaleElementRefEx.java
+++ src/test/java/com/cydeo/tests/day5_testing_Intro_dropdowns/T1_StaleElementRefEx.java
@@ -21,8 +21,6 @@ public class T1_StaleElementRefEx {
         //2. Go to https://practice.cydeo.com/add_remove_elements/
         driver.get("https://practice.cydeo.com/add_remove_elements/");
 
-
-
         //3. Click to “Add Element” button
         //button[@onclick='addElement()']
         //GoalIsToLocateTheWebElementClickOnAddElement+InspectTo LocateTheWebElementThatReturnsMe
@@ -30,32 +28,41 @@ public class T1_StaleElementRefEx {
         //thenAdd-->button & MyText or xpath stands for text =2AddElement-->By.xpath("//button[.='Add Element']"));
         WebElement addElementBtn = driver.findElement(By.xpath("//button[.='Add Element']"));
 
-        //Don'tNeedToAddThread.sleep <--OnlyAddToSeeSleepFor2Seconds
-        Thread.sleep(2000);
+        Thread.sleep(2000);  //Don'tNeedToAddThread.sleep <--OnlyAddToSeeSleepFor2Seconds
         addElementBtn.click();
 
         //4. Verify “Delete” button is displayed after clicking.
+        //NextLocateTheWQebElement EfalaMeDarOBotaoOnde whereTheClassValueIsThis-->"//button[@class='added-manually']"));
+        //IfTheDeleteButtonWasRemovedOnTheWeb MeansItWasThereButThenOnceDeleted and sinceThereIsNotElement
+        //NoteOnIntellij saysStaleElementReferenceException, So SinceThereIsNotElementTheOnlyOption isToSolveWith
+        //Try Catch Go To Line--->52, whereThrough theException
         WebElement deleteButton = driver.findElement(By.xpath("//button[@class='added-manually']"));
 
         System.out.println("deleteButton.isDisplayed() = " + deleteButton.isDisplayed());
 
         //5. Click to “Delete” button.
+        //OnceIRunTheCodeWaitFor2secondOnceI click andGoBackToMyIntellij IShouldSeeTrue
         deleteButton.click();
 
         //6. Verify “Delete” button is NOT displayed after clicking.
-
-        try {
+        //IfIsNotDisplayItShouldReturnFalse
+        //HowToSolveTheException,
+        try {     //ICanSayTryThisCode below
 
             System.out.println("deleteButton.isDisplayed() = " + deleteButton.isDisplayed());
 
-        }catch (StaleElementReferenceException e){
+        }catch (StaleElementReferenceException e){ // CatchMe-->}catch (StaleElementReferenceException e){
 
+            //ThenYouCanWriteSomething ToExplainToYourSelf onYour First PrintStatementBelow
+            //Then YouCanGive explanation on your 2rd print statement
+            //Third YouCanWrite false coz If is comeToThis block & if is deleted from the page is not display anymore
             System.out.println("-->StaleElementReferenceException exception is thrown");
             System.out.println("-->This means the web element is completely deleted from the page");
             System.out.println("deleteButton.isDisplayed() = false");
 
         }
 
+        //U Can Close
         driver.close();
     }
 }
