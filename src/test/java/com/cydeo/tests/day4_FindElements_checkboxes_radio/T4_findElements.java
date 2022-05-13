be8f7275bb0c98a6908a061c8584d1d3405b72da
mariajoana <gokalp.maryam@gmail.com>
2022-05-13 11:18:40 +0100

    day4_T4_findElements

--- src/test/java/com/cydeo/tests/day4_FindElements_checkboxes_radio/T4_findElements.java
+++ src/test/java/com/cydeo/tests/day4_FindElements_checkboxes_radio/T4_findElements.java
@@ -1,6 +1,6 @@
 package com.cydeo.tests.day4_FindElements_checkboxes_radio;
 
-import com.cydeo.reviewWithOscar.week2.WebDriverFactory;
+import com.cydeo.tests.utilities.WebDriverFactory;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
