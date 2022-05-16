3988c1c5820dcd8f31335d338faa93e8cdac9c1d
mariajoana <gokalp.maryam@gmail.com>
2022-05-16 01:35:03 +0100

    day06_T1_Alert_Practices

--- src/test/java/com/cydeo/tests/day6/T1_Alert_Practices.java
+++ src/test/java/com/cydeo/tests/day6/T1_Alert_Practices.java
@@ -16,7 +16,7 @@ public class T1_Alert_Practices {
     WebDriver driver;
 
     @BeforeMethod
-    public void setupMethod(){
+    public void setupMethod() {
         //1. Open browser
         driver = WebDriverFactory.getDriver("chrome");
         driver.manage().window().maximize();
@@ -53,7 +53,80 @@ public class T1_Alert_Practices {
         String actualText = resultText.getText();
 
         Assert.assertEquals(actualText, expectedText, "Actual result text is not as expected!!!");
-    }
 
+
+        @Test
+        class TC2_ConfirmationAlertPractice {
+            public void setDriver(WebDriver driver) throws InterruptedException {
+
+                // TC #2: Confirmation alert practice
+                //1. Open browser
+                driver = WebDriverFactory.getDriver("chrome");
+                driver.manage().window().maximize();
+                driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+                //2. Go to website: http://practice.cydeo.com/javascript_alerts
+                driver.get("http://practice.cydeo.com/javascript_alerts");
+
+                // 3. Click to “Click for JS Confirm” button
+                WebElement informationAlertButton = driver.findElement(By.xpath("//button[.='Click for JS Alert']"));
+
+                informationAlertButton.click();
+                Thread.sleep(1000);
+
+                //4. Click to OK button from the alert
+                alert.accept();
+
+                //5. Verify “You clicked: Ok” text is displayed.
+                //OnWebType-->//p[@id='result'] <--LocateCopy&PastCodeBelow
+                WebElement resultText = driver.findElement(By.xpath("//p[@id='result']"));
+
+                //VerificationBelow
+                String expectedText = "You successfully clicked an alert";
+                String actualText = resultText.getText();
+
+                Assert.assertEquals(actualText, expectedText, "Actual result text is not as expected!!!");
+
+            }
+
+            WebDriver driver;
+
+            @Test
+            class TC3_InformationAlertPractice {
+                public void setDriver(WebDriver driver) throws InterruptedException {
+                    //TC #3: Information alert practice
+                    //1. Open browser
+                    driver = WebDriverFactory.getDriver("chrome");
+                    driver.manage().window().maximize();
+                    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+                    //2. Go to website: http://practice.cydeo.com/javascript_alerts
+                    driver.get("http://practice.cydeo.com/javascript_alerts");
+
+                    //3. Click to “Click for JS Prompt” button
+                    WebElement informationAlertButton = driver.findElement(By.xpath("//button[.='Click for JS Prompt']"));
+                    informationAlertButton.click();
+                    Thread.sleep(1000);
+
+                    //4. Send “hello” text to alert
+                    alert.accept();
+
+                    //5. Click to OK button from the alert
+                    //OnWebType-->//p[@id='Result Alert'] <--LocateCopy&PastCodeBelow
+
+                    WebElement resultText = driver.findElement(By.xpath("//p[@id='Result Alert']"));
+
+                    //6. Verify “You entered:  hello” text is displayed.
+                    String expectedText = "You successfully clicked an alert";
+                    String actualText = resultText.getText();                                                                                                          
+                    Assert.assertEquals(actualText, expectedText, "Actual result text is not as expected!!!");
+
+                }
+
+            }
+
+        }
+
+    }
 
 }
