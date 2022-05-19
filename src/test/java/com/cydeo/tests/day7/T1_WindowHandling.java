4b58baa618794044c860b459e13a0a22f6acbe9f
mariajoana <gokalp.maryam@gmail.com>
2022-05-19 10:08:39 +0100

    day7_T1_WindowHandling

--- src/test/java/com/cydeo/tests/day7/T1_WindowHandling.java
+++ src/test/java/com/cydeo/tests/day7/T1_WindowHandling.java
@@ -7,7 +7,11 @@ import org.openqa.selenium.WebDriver;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
+
+//thisIsHowToHandleFourTestCaseScenarioBelow
+//JavaScript Selenium Library Help YouTpRunYourTestCases at Once
 
 public class T1_WindowHandling {
 
@@ -27,11 +31,39 @@ public class T1_WindowHandling {
         driver.get("https://www.amazon.com");
 
         //3. Copy-paste the lines from below into your class
+        //HereBelow We are downCasting Casting-->driver Type To The-->(JavascriptExecutor)
+        //OtherWiseWeWontBeAbleToReachThis-->executeScript<--topic
+        //FunctionWeUseJavaScript()-->window.open inside-->
         ((JavascriptExecutor) driver).executeScript("window.open('https://google.com','_blank');");
         ((JavascriptExecutor) driver).executeScript("window.open('https://etsy.com','_blank');");
         ((JavascriptExecutor) driver).executeScript("window.open('https://facebook.com','_blank');");
 
         //4. Create a logic to switch to the tab where Etsy.com is open
+        //CreateSetOfString1st&ISayDriverGetWindowHandle--> Set<String> allWindowHandles = driver.getWindowHandles();
+       //Iter+Enter4ForEachLoop.INeed2LookThroughIsWidowHandle-->for(String windowHandle :driver.getWindowHandles()){
+        //then I Can Say Driver Switch To Window Each-->driver.switchTo().window(each);
+        //-->GetCurrentURLICanSay-->CurrentURL:"+ driver.getCurrentUrl
+        // PrintToGet&SeeWhereIsTheCurrentURL-->System.out.println("Current URL: " + driver.getCurrentUrl());
+        //ToStopSwitchWeCanSayIsDriverGetTheCurrent URL-->if ( (driver.getCurrentUrl().contains("etsy"))){
+        //BreakALoopForContinues I stay on This pageApplyBreak-->break;
+
+        Set<String> allWindowHandles = driver.getWindowHandles();
+
+        for (String each : allWindowHandles){
+
+            driver.switchTo().window(each);
+
+            System.out.println("Current URL: " + driver.getCurrentUrl());
+
+            if ( (driver.getCurrentUrl().contains("etsy"))){
+                break;
+            }
+
+
+        }
+
+        //5. Assert.Title contains "Etsy"
+        //Lines to be posted
 
         BrowserUtils.switchWindowAndVerify(driver, "etsy", "Etsy");
     }
