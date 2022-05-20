81c5051eb177c9571283993fb52a0af14d3811cd
mariajoana <gokalp.maryam@gmail.com>
2022-05-20 10:09:27 +0100

    utilities_BrowserUtils

--- src/test/java/com/cydeo/tests/utilities/BrowserUtils.java
+++ src/test/java/com/cydeo/tests/utilities/BrowserUtils.java
@@ -1,8 +1,8 @@
 package com.cydeo.tests.utilities;
 
-/*
-In this class only general utility methods that are not related to some specific page.
- */
+  /*
+   In this class only general utility methods that are not related to some specific page.
+   */
 
 import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
@@ -12,7 +12,8 @@ import java.util.Set;
 public class BrowserUtils {
 
     /*
-    //AlwaysExplainWhatThe()isDoing whenEverUCreateAClass
+    Don't mention thread.sleep =() to handle 1or2second pause but u can still use at work
+    AlwaysExplainWhatThe()isDoing whenEverUCreateAClass
     This method below will accept int (in seconds) and execute Thread.sleep
     for given duration
      */
@@ -25,14 +26,39 @@ public class BrowserUtils {
         }
     }
 
+    //Method below
+    //WhenEver I passMy Inpected title It will be passed from the public class name variable-->String expectedInUrl
+    // into the I Created 3 argument first argument I pass my driver second argument I pass my actual title coming
+    // from the dynamic getTitle();
+    //3ArgumentCreated 1st argument pass my driver-->WebDriver driver. Second argument passURLCondition
+    //trying expectedInUrl and 3rd argument pass my-->String expectedInTitle){
+    //MY FOR EACH LOOP STEPS
+    //CreateSetOfString1st&ISayDriverGetWindowHandle--> Set<String> allWindowHandles = driver.getWindowHandles();
+    //Iter+Enter4ForEachLoop.INeed2LookThroughIsWidowHandle-->for(String windowHandle :driver.getWindowHandles()){
+    //then I Can Say Driver Switch To Window Each-->driver.switchTo().window(each);
+    //-->GetCurrentURLICanSay-->CurrentURL:"+ driver.getCurrentUrl
+    // PrintToGet&SeeWhereIsTheCurrentURL-->System.out.println("Current URL: " + driver.getCurrentUrl());
+    //ToStopSwitchWeCanSayIsDriverGetTheCurrent URL-->if ( (driver.getCurrentUrl().contains("etsy"))){
+    //BreakALoopForContinues I stay on This pageApplyBreak-->break;
+    //Our GoalIsTo2understand4UsToBeAbleToSwitchWeCreateForLoop-- driver.switchTo().window(each);
+    //WindowHandleWeCan'tUseToDetermineWhichBrowserShouldBeStopping
+    //IsThat'sWhyWeAreUsingURL4That--> System.out.println("Current URL: " + driver.getCurrentUrl());
+    // WeSayIfMyURLContainSomethingJustBreakTheLoopCozThisIsMyURL&IamOnMyexpectedPag------>break;
 
-    /*
-    This method accepts 3 arguments.
+     /*
+    This method below accepts 3 arguments.
     Arg1: webdriver
     Arg2: expectedInUrl : for verify if the url contains given String.
         - If condition matches, will break loop.
     Arg3: expectedInTitle to be compared against actualTitle
      */
+
+    /*
+     TheMore Utility we create the less content we have on the test itself coz the plan is to remove
+    as much lime From the test itself to make as simple and controlled from the outside
+    Why create a method Static? Because I want to make as simple as possible coz I want to say driver.util & verify
+     */
+    //Create a new method name called switching window and verify
     public static void switchWindowAndVerify(WebDriver driver, String expectedInUrl, String expectedInTitle){
 
         Set<String> allWindowsHandles = driver.getWindowHandles();
@@ -49,16 +75,22 @@ public class BrowserUtils {
         }
 
         //5. Assert:Title contains “expectedInTitle”
+        //ToGetActual Title we say-->  Assert.assertTrue(actualTitle.contains(expectedInTitle));
         String actualTitle = driver.getTitle();
         Assert.assertTrue(actualTitle.contains(expectedInTitle));
     }
 
     /*
     This method accepts a String "expectedTitle" and Asserts if it is true
+
+     To get the title we could say
+     Assert.assertEquals(driver.getTitle(), expectedTitle);
      */
+    //WeCouldSayThis--> Assert.assertEquals(driver.getTitle(), expectedTitle);<--InsideTheMethod
     public static void verifyTitle(WebDriver driver ,String expectedTitle){
 
         Assert.assertEquals(driver.getTitle(), expectedTitle);
 
     }
 }
+
