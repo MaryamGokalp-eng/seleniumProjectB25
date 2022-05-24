b2995c4ef203d09b37ee8ef7cb595176fc112450
mariajoana <gokalp.maryam@gmail.com>
2022-05-24 11:18:54 +0100

    day8_LearningProperties

---
+++ src/test/java/com/cydeo/tests/day8_Properties_config_reader/LearningProperties.java
@@ -0,0 +1,20 @@
+package com.cydeo.tests.day8_Properties_config_reader;
+
+import org.testng.annotations.Test;
+
+public class LearningProperties {
+
+    @Test
+    public void java_properties_reading_test(){
+        //This Class allow us to get value from the property to get the userName of yu computer
+        //This argument below you give the key & provide you the value.
+        // You pass the Key is -->: os.name and returns you the value-->Mac OS X
+        //key = value
+        //os.name = Mac OS X
+        //user.name = CyberTekChicago-1
+        System.out.println("System.getProperty(\"os.name\") = " + System.getProperty("os.name"));
+        System.out.println("System.getProperty(\"user.name\") = " + System.getProperty("user.name"));
+
+    }
+
+}
