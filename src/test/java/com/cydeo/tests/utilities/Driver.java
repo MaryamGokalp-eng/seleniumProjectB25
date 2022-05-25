72ac2dca85e3b188f146b54911001ec88bdc0628
mariajoana <gokalp.maryam@gmail.com>
2022-05-25 13:46:54 +0100

    Driver

--- src/test/java/com/cydeo/tests/utilities/Driver.java
+++ src/test/java/com/cydeo/tests/utilities/Driver.java
@@ -7,6 +7,11 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 
 import java.util.concurrent.TimeUnit;
 
+//WhyCreate DriverClassInUtility? Coz WebDriverClass CanNot Handle,WriteALotOfLine, toBeAbleToInitiate our WebDriver
+//Have hardTime to pass the same driver instance around ourProject, WhenWeAreUsingUtility() WeHaveToPass
+//WebDriver driver as the argument in each utility method. Is that's wht we created a Driver logic to guarantee
+// the same exact instance every time we call the method
+
 public class Driver {
 
     /*
@@ -24,21 +29,36 @@ public class Driver {
     /*
     Create a re-usable utility method which will return same driver instance when we call it
      */
+
     public static WebDriver getDriver(){
 
-        if (driver == null){
+        if (driver == null) {
 
             /*
-            We read our browserType from configuration.properties.
-            This way, we can control which browser is opened from outside our code, from configuration.properties.
+            FIRST APPROACH
+        //The if condition below means that if our driver is not created it will create one for us & if is
+        // created it will return me the existing one and U can use in whatEver U need to use.
+        //Only thing is you need to be precise in what you are doing then you can apply the logic
+
+        //We read our browserType from configuration.properties.
+        //This way, we can control which browser is opened from outside our code, from configuration.properties.
+
+                    String browserType = ConfigurationReader.getProperty("browser");
+                      }
+                        return driver;
+                      }
+                   }
              */
             String browserType = ConfigurationReader.getProperty("browser");
-
 
             /*
                 Depending on the browserType that will be return from configuration.properties file
                 switch statement will determine the case, and open the matching browser
             */
+
+            //IfTheCompanyYouWorkingFor don't Allow youToUse <dependency> bonigarcia<. You CanDownload WebDriver
+            //Manually from Google & adding into the project.
+            //BELOW SHOW YOU HOW TO ADD CODE MANUALLY FROM TheWebDriver
             switch (browserType){
                 case "chrome":
                     WebDriverManager.chromedriver().setup();
@@ -46,6 +66,9 @@ public class Driver {
                     driver.manage().window().maximize();
                     driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
                     break;
+
+            // AGAIN BELOW ARE DIFFERENT METHOD ON HOW TO ADD DIFFERENT () MANUALLY U CanCopy&Paste&ChangeTheName
+            //From chrome to FireFox
                 case "firefox":
                     WebDriverManager.firefoxdriver().setup();
                     driver = new FirefoxDriver();
