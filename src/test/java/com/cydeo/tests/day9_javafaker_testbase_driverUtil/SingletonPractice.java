ed243777ddf3b3d6bf7c4d0cc07c70b5708cb482
mariajoana <gokalp.maryam@gmail.com>
2022-05-25 14:55:11 +0100

    day9_SingletonPractice

--- src/test/java/com/cydeo/tests/day9_javafaker_testbase_driverUtil/SingletonPractice.java
+++ src/test/java/com/cydeo/tests/day9_javafaker_testbase_driverUtil/SingletonPractice.java
@@ -8,8 +8,7 @@ public class SingletonPractice {
     public void singleton_understand_test1(){
 
         //The method below copy the Line & paste two time change the numbers from str1 to str2 & str3
-        //
-
+        //When It assign the value one time & when I call the method it will return the value
         String str1 = Singleton.getWord();
         System.out.println("str1 = " + str1);
 
@@ -20,7 +19,7 @@ public class SingletonPractice {
         System.out.println("str3 = " + str3);
 
     }
-
+    //One more test below & when you change the priority it will return same ID. If does not exit return the value
     @Test (priority = 1)
     public void singleton_understanding_test2(){
         String str4 = Singleton.getWord();
