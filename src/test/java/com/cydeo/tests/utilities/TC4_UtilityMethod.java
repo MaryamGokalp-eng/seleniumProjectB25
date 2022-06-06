3465c8bb51be62931ef955965652b11220a1eb7e
mariajoana <gokalp.maryam@gmail.com>
2022-06-06 19:04:54 +0100

    utilities_TC4_UtilityMethod

---
+++ src/test/java/com/cydeo/tests/utilities/TC4_UtilityMethod.java
@@ -0,0 +1,40 @@
+package com.cydeo.tests.utilities;
+
+public class TC4_UtilityMethod {
+
+    //TC #4: Create utility method
+    // 1. Create a new method for login
+
+
+
+    //2. Create a method to make Task3 logic re-usable
+
+
+
+    //3. When method is called, it should simply login
+
+
+
+
+
+    //This method should have at least 2 overloaded versions.
+    //Method #1 info:
+    //• Name: login_crm()
+    //• Return type: void
+    //• Arg1: WebDriver
+
+
+
+
+
+    //Method #2 info:
+    //• Name: login_crm()
+    //• Return type: void
+    //• Arg1: WebDriver
+    //• Arg2: String username
+    //• Arg3: String password
+
+
+
+
+}
