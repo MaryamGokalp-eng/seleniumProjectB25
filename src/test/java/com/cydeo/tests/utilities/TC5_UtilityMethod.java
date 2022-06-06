68057df7cf67c926a9b2e7feda5f02296ffcf47c
mariajoana <gokalp.maryam@gmail.com>
2022-06-06 19:04:30 +0100

    utilities_TC5_UtilityMethod

---
+++ src/test/java/com/cydeo/tests/utilities/TC5_UtilityMethod.java
@@ -0,0 +1,24 @@
+package com.cydeo.tests.utilities;
+
+public class TC5_UtilityMethod {
+    //TC #5: Create utility method
+    // 1. Create a new method for title verification
+
+
+    //2. Create a method to make title verification logic re-usable
+
+
+
+
+    //3. When method is called, it should simply verify expected title with actual title
+
+
+
+    //Method info:
+    //• Name: verifyTitle()
+    //• Return type: void
+    //• Arg1: WebDriver
+    //• Arg2: String expectedTitle
+
+
+}
