45bf64f946983d63fbb71e5dfdfde05e08888eef
mariajoana <gokalp.maryam@gmail.com>
2022-06-07 23:12:43 +0100

    avengerHours_SearchBlue

---
+++ src/test/java/com/cydeo/reviewWithOscar/avengerHours/SearchBlue.java
@@ -0,0 +1,38 @@
+package com.cydeo.reviewWithOscar.avengerHours;
+
+import com.cydeo.tests.utilities.Driver;
+import com.cydeo.tests.utilities.ReviewUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+public class SearchBlue extends TestBaseForAvengers {
+
+    @Test
+    public void Test(){
+
+        // click products
+        ReviewUtils.getLink("Products");
+
+        WebElement searchBox = Driver.getDriver().findElement(By.id("search_product"));
+
+        WebElement searchButton = Driver.getDriver().findElement(By.id("submit_search"));
+        searchBox.sendKeys("blue"); // search and enter
+        searchButton.click();
+
+        // get the names of products that comes with search
+
+        List<WebElement> elementsList = Driver.getDriver().findElements(By.xpath("//img[contains(@src,'product_picture')]/../p"));
+
+        for (WebElement webElement : elementsList) {
+            System.out.println("webElement.getText() = " + webElement.getText());
+            Assert.assertTrue(webElement.getText().toLowerCase().contains("blue"));
+        }
+
+    }
+
+
+}
