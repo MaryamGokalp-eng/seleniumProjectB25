176a2085c989b18869f6e3e9d916df42256f3f06
mariajoana <gokalp.maryam@gmail.com>
2022-06-08 13:52:09 +0100

    avengerHours

--- src/test/java/com/cydeo/reviewWithOscar/avengerHours/SearchBlue.java
+++ src/test/java/com/cydeo/reviewWithOscar/avengerHours/SearchBlue.java
@@ -1,7 +1,7 @@
 package com.cydeo.reviewWithOscar.avengerHours;
 
-import com.cydeo.tests.utilities.Driver;
-import com.cydeo.tests.utilities.ReviewUtils;
+import com.cydeo.utilities.Driver;
+import com.cydeo.utilities.ReviewUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
