ea546972f5bd6f9851887e78ef75b26fbfe93453
mariajoana <gokalp.maryam@gmail.com>
2022-06-08 13:48:40 +0100

    Pages

--- src/test/java/com/cydeo/Pages/LibraryLoginPage.java
+++ src/test/java/com/cydeo/Pages/LibraryLoginPage.java
@@ -1,6 +1,6 @@
 package com.cydeo.Pages;
 
-import com.cydeo.tests.utilities.Driver;
+import com.cydeo.utilities.Driver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
