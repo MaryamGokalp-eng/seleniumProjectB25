6b8d28eeb66984526b62ec946104e821d152da75
mariajoana <gokalp.maryam@gmail.com>
2022-06-08 13:35:07 +0100

    day1_selenium_intro

--- src/test/java/com/cydeo/tests/tests/tests/day1_selenium_intro/SeleniumTest.java
+++ src/test/java/com/cydeo/tests/tests/tests/day1_selenium_intro/SeleniumTest.java
@@ -1,4 +1,4 @@
-package com.cydeo.tests.day1_selenium_intro;
+package com.cydeo.tests.tests.tests.day1_selenium_intro;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
