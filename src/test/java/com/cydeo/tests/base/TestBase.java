edea011991561483c88fcf262ac35f7d564ed7b0
mariajoana <gokalp.maryam@gmail.com>
2022-06-08 14:05:24 +0100

    base

--- src/test/java/com/cydeo/tests/base/TestBase.java
+++ src/test/java/com/cydeo/tests/base/TestBase.java
@@ -1,6 +1,6 @@
 package com.cydeo.tests.base;
 
-import com.cydeo.tests.utilities.WebDriverFactory;
+import com.cydeo.utilities.WebDriverFactory;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
