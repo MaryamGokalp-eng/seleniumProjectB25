f4aa56d52c59f6df09db1b0a0b5aa803cb22a3ff
mariajoana <gokalp.maryam@gmail.com>
2022-06-09 13:37:47 +0100

    day12_POMPractices

--- src/test/java/com/cydeo/tests/tests/tests/day12_pom_design_explicit_wait/POMPractices.java
+++ src/test/java/com/cydeo/tests/tests/tests/day12_pom_design_explicit_wait/POMPractices.java
@@ -21,8 +21,11 @@ public class POMPractices {
         //TC #1: Required field error message test
         //1- Open a Chrome browser
         //2- Go to: https://library1.cydeo.com/
+        Driver.getDriver().get("https://library1.cydeo.com/");
+
         //3- Do not enter any information
         //4- Click to “Sign in” button
+        libraryLoginPage = new LibraryLoginPage();
 
         libraryLoginPage.signInButton.click();
 
@@ -38,7 +41,10 @@ public class POMPractices {
         //TC #2: Invalid email format error message test
         //1- Open a Chrome browser
         //2- Go to: https://library1.cydeo.com/
+        Driver.getDriver().get("https://library1.cydeo.com");
+
         //3- Enter invalid email format
+        libraryLoginPage = new LibraryLoginPage();
 
         libraryLoginPage.inputUsername.sendKeys("somethingwrong");
 
@@ -48,6 +54,7 @@ public class POMPractices {
         //Expected: Please enter a valid email address.
         Assert.assertTrue(libraryLoginPage.enterValidEmailErrorMessage.isDisplayed());
 
+        //This() call the driverQuite to terminate the whole session
         Driver.closeDriver();
 
     }
