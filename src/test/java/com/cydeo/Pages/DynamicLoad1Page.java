3499dbef95310222b72d6001f4cd7d795a5f80f2
mariajoana <gokalp.maryam@gmail.com>
2022-06-11 10:03:54 +0100

    Pages_DynamicLoad1Page

--- src/test/java/com/cydeo/Pages/DynamicLoad1Page.java
+++ src/test/java/com/cydeo/Pages/DynamicLoad1Page.java
@@ -6,15 +6,15 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class DynamicLoad1Page {
-
+    // Set My class constructor for page object modelForMyDynamicClassDay12--> public void dynamic_load_1_test
     public DynamicLoad1Page(){
         PageFactory.initElements(Driver.getDriver(), this);
     }
 
-    @FindBy(xpath = "//button[.='Start']")
-    public WebElement startButton;
+    @FindBy(xpath = "//button[.='Start']")          //Locator
+    public WebElement startButton;                  //1st LocateStartButtonOnWebElement & DecideWhichWebElement2Use
 
-    @FindBy(css = "div#loading")
+    @FindBy(css = "div#loading")                    //GetMeTheDivWhereMyIDIsLoading-->(css = "div#loading")
     public WebElement loadingBar;
 
     //@FindBy(id = "username")
